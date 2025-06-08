a3525d468515a85dd5f64edb339ee527365df3a1
Hao_Tian22 <hao_tian22@outlook.com>
2025-06-08 22:40:48 +0800

    Refactor code structure for improved readability and maintainability

--- src/Netease.py
+++ src/Netease.py
@@ -1,17 +1,11 @@
 import requests
-# import logging
-# import threading
-# import flet as ft
-from NeteaseCloudMusic import NeteaseCloudMusicApi, api_help, api_list
-
-netease_cloud_music_api = NeteaseCloudMusicApi()  # 初始化API
-version_result = netease_cloud_music_api.request("inner_version")
+from pyncm import apis
 # print(
     # f'当前使用NeteaseCloudMusicApi版本号：{version_result["NeteaseCloudMusicApi"]}\n当前使用NeteaseCloudMusicApi_V8版本号：{version_result["NeteaseCloudMusicApi_V8"]}')  # 退出登录
 
 def search(search_value):
     # search_value = input("请输入要搜索的内容：")  # 输入搜索内容
-    response = netease_cloud_music_api.request("/cloudsearch",{"keywords":search_value})  # 请求API
+    response = []
     # print(response)  # 打印返回结果
     ''''''
     try:
@@ -22,11 +16,7 @@ def search(search_value):
         result_song_list = result_list["songs"] 
         mode = 1
     except TypeError:
-        response = requests.get("https://neteasecloudmusicapi.vercel.app/search?keywords="+search_value).json()
-        result_list = response["result"]
-        if result_list["songCount"] == 0:
-            return []
-        result_song_list = result_list["songs"]
+        result_song_list = apis.cloudsearch.GetSearchResult(search_value)["result"]["songs"]  # 获取歌曲列表
         mode = 2
     # song_objects = ft.Column(controls=[],scroll="auto",height=520,width=1020,horizontal_alignment=ft.CrossAxisAlignment.CENTER)
     songs_list = []
@@ -41,10 +31,10 @@ def search(search_value):
         if mode == 2:
             song_name = song["name"]
             song_id = song["id"]
-            song_album = song["album"]["name"]
-            song_singer = song["artists"][0]["name"]
-            song_album_id = song["album"]["id"]
-            song_auxiliary = song["alias"][0] if ("alias" in song and len(song["alias"])>0) else ""
+            song_album = song["al"]["name"]
+            song_singer = song["ar"][0]["name"]
+            song_album_id = song["al"]["id"]
+            song_auxiliary = song["tns"][0] if ("tns" in song and len(song["tns"])>0) else ""
         songs_list.append({"song_name":song_name,"song_id":song_id,"song_album":song_album,"song_singer":song_singer,"song_album_id":song_album_id,"song_auxiliary":song_auxiliary})
         # song_objects.controls.append(Song(song_name, song_id, song_album, song_singer, song_album_id, song_auxiliary).ui)
     # Only UI
@@ -52,27 +42,28 @@ def search(search_value):
 
 def get_url(song_id,quality="higher"):
     if quality == "Standard":
-        quality = 'standard'
+        quality = '96000'
     elif quality == "Higher":
-        quality = 'higher'
+        quality = '320000'
     elif quality == "Ex-High":
-        quality = 'exhigh'
+        quality = '320000'
     elif quality == "Lossless":
         quality = 'lossless'
-    response = netease_cloud_music_api.request("/song/url/v1",{"id":song_id,"level":quality})
-    url = response["data"]["data"][0]["url"]
+    response = apis.track.GetTrackAudio(song_id, quality)
+    url = response["data"][0]["url"]
     return url
 
 def get_info(songid):
-    response = netease_cloud_music_api.request("/song/detail",{"ids":str(songid)})
+    response = apis.track.GetTrackDetail(songid)
     # response = requests.get("https://neteasecloudmusicapi.vercel.app/song/detail?ids="+songid).json()
     # pic_url = response["data"]["songs"][0]["al"]["picUrl"]
     # pic = requests.get(pic_url).content
     response_data = response["data"]["songs"][0]
+    print(response_data)
     info = {"song_name":response_data["name"],"song_id":songid,"song_album":response_data["al"]["name"],"song_singer":response_data["ar"][0]["name"],"song_album_id":response_data["al"]["id"],"pic_url":response_data["al"]["picUrl"]}
     return info
 
 def get_lyric(songid):
-    response = netease_cloud_music_api.request("/lyric",{"id":str(songid)})
-    lyric = response["data"]["lrc"]["lyric"]
+    response = apis.track.GetTrackLyrics(songid)
+    lyric = response["lrc"]["lyric"]
     return lyric
